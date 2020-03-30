3f6a7bb3f0cec1c19bc76e4d0c8ba9819647895a
Baptiste Iafrate <baptiste.iafrate@gmail.com>
2020-03-31 01:50:48 +0200

    issue extractor

--- routes/api/jql.js
+++ routes/api/jql.js
@@ -33,6 +33,28 @@ const getWorklog = worklogs => {
   return cleanWorklog;
 };
 
+// Create issue extractor function
+const getIssues = issues => {
+  const cleanIssues = [];
+  for (const issue of issues) {
+    cleanIssues.push({
+      key: issue.key,
+      summary: issue.fields.summary,
+      status: issue.fields.status.name,
+      issuetype: issue.fields.issuetype.name,
+      affectsVersion: issue.fields.versions.name,
+      fixVersion: issue.fields.fixVersions,
+      epicLink: issue.fields.customfield_10006,
+      SP: issue.fields.customfield_10002,
+      SP_FE: issue.fields.customfield_10700,
+      SP_BE: issue.fields.customfield_10701,
+      components: issue.fields.components.name,
+      worklog: getWorklog(issue.fields.worklog.worklogs)
+    });
+  }
+  return cleanIssues;
+};
+
 //@route POST api/jql
 //@ desc searches Diabolocom jira for tickets matching jql query
 // @access Private (Jira auth)
@@ -67,15 +89,17 @@ router.post(
           'customfield_10006',
           'customfield_10700',
           'customfield_10701',
+          'customfield_10002',
           'worklog'
         ]
       });
       console.log(response.data);
       res.status(200);
 
-      const cleanResponse = {};
+      const cleanResponse = getIssues(response.data.issues);
+      res.json(cleanResponse);
 
-      res.json(response.data);
+      //res.json(response.data);
       //res.send('jql route');
     } catch (err) {
       console.error(err.message);
