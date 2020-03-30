02092caeece27ff66b02f673850ffe68458c0df7
Baptiste Iafrate <baptiste.iafrate@gmail.com>
2020-03-30 12:41:39 +0200

    check jql input

--- routes/api/jql.js
+++ routes/api/jql.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
 //@route POST api/jql
 //@ desc searches Diabolocom jira for tickets matching jql query
 // @access Private (Jira auth)
-router.post('/', (req, res) => res.send('jql route'));
+router.post(
+  '/',
+  [
+    check('jql', 'JQL is required')
+      .not()
+      .isEmpty()
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+    res.send('jql route');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 
 app.get('/', (req, res) => res.send('API running'));
 
+// Init Middleware
+app.use(express.json({ extended: false }));
+
 // Define Routes
 app.use('/api/jql', require('./routes/api/jql'));
 app.use('/api/worklog', require('./routes/api/worklog'));
