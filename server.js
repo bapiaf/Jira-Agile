2725f33fdd69e7b507b60c13e381c36967f45c5f
Baptiste Iafrate <baptiste.iafrate@gmail.com>
2021-06-08 17:57:37 +0200

    cleaning up

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ app.use(express.json({ extended: false }));
 
 // Define Routes
 app.use('/api/qa', require('./routes/api/qa'));
-app.use('/api/hello', require('./routes/api/hello'));
-app.use('/api/jql', require('./routes/api/jql'));
-app.use('/api/jql2', require('./routes/api/jql2'));
-app.use('/api/jql3', require('./routes/api/jql3'));
 app.use('/api/jql4', require('./routes/api/jql4'));
 
 const PORT = process.env.PORT || 5000;
